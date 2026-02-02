e199ae46b02bd57ae64109fe456b76dd90df30c8
Bruno Escalhão <brunoescalhao@gmail.com>
2026-02-02 07:31:04 -0300

    CI/Teste Básicos

---
+++ tests/test_basic.py
@@ -0,0 +1,8 @@
+def test_importa_engine():
+      from src import engine
+      assert engine is not None
+
+
+  def test_importa_scoring():
+      from src import scoring
+      assert scoring is not None
